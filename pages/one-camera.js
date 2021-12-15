08e632d9e4564104ade9b97620a2d72fe956dc28
Alex Trost <19617280+a-trost@users.noreply.github.com>
2021-12-15 15:29:44 -0500

    Add one camera scene

--- pages/one-camera.js
+++ pages/one-camera.js
@@ -1,6 +1,18 @@
 // Scene for only full-screen host
-import React from "react";
+import styles from "../styles/OneCamera.module.css";
+import Logo from "../components/Logo";
+import LatestDonations from "../components/LatestDonations";
+import ProgressBar from "../components/ProgressBar";
+import Sponsors from "../components/Sponsors";
 
 export default function OneCamera() {
-  return <div>OneCamera</div>;
+  return (
+    <div className="scene">
+      <div className={styles.host} />
+      <Logo height={154} width={260} className={styles.logo} />
+      <Sponsors className={styles.sponsors} />
+      <ProgressBar className={styles.progressBar} />
+      <LatestDonations className={styles.latestDonations} />
+    </div>
+  );
 }
