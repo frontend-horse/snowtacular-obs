1142d0c770a83403288b5309cc082047aee0f49c
Alex Trost <19617280+a-trost@users.noreply.github.com>
2021-12-14 19:29:29 -0500

    Small tweaks to sponsor component

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -1,29 +1,28 @@
-import styles from '../styles/Sponsors.module.css';
+import styles from "../styles/Sponsors.module.css";
 
-const sponsors = [
-	"prismic",
-	"cloudinary",
-	"netlify",
-]
+const sponsors = ["prismic", "cloudinary", "netlify"];
 
-export default function Sponsors() {
-	if(typeof window !== 'undefined') {document.documentElement.style.setProperty('--num-sponsors', sponsors.length);}
+export default function Sponsors({ className }) {
+  if (typeof window !== "undefined") {
+    document.documentElement.style.setProperty(
+      "--num-sponsors",
+      sponsors.length
+    );
+  }
 
-	return(
-		<div className={styles.container}>
-			<p>
-				All your donations are being matched by our sponsors
-			</p>
-			<div className={styles.cropBox}>
-				<div className={styles.images}>
-					{sponsors.map((sponsor) => (
-						<div key={sponsor}>
-							{/* eslint-disable-next-line @next/next/no-img-element */}
-							<img src={`sponsors/${sponsor}.svg`} alt=""/>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
-	)
-};
+  return (
+    <div className={`${styles.container} ${className}`}>
+      <p>All your donations are being matched by our sponsors</p>
+      <div className={styles.cropBox}>
+        <div className={styles.images}>
+          {sponsors.map((sponsor) => (
+            <div key={sponsor}>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src={`sponsors/${sponsor}.svg`} alt="" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
