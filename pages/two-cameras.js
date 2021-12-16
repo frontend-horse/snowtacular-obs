2fa1dd9bd38560082d87679511d97ecd1a786574
Alex Trost <19617280+a-trost@users.noreply.github.com>
2021-12-15 21:15:17 -0500

    Set up two camera page

--- pages/two-cameras.js
+++ pages/two-cameras.js
@@ -1,6 +1,19 @@
 // Scene for Host + Guest cameras
-import React from "react";
+import Logo from "../components/Logo";
+import LatestDonations from "../components/LatestDonations";
+import ProgressBar from "../components/ProgressBar";
+import Sponsors from "../components/Sponsors";
+import styles from "../styles/TwoCameras.module.css";
 
 export default function TwoCameras() {
-  return <div>Two Cameras</div>;
+  return (
+    <div className="scene">
+      <div className={styles.host} />
+      <div className={styles.guest} />
+      <Logo height={154} width={260} className={styles.logo} />
+      <Sponsors className={styles.sponsors} />
+      <LatestDonations className={styles.latestDonations} />
+      <ProgressBar className={styles.progressBar} />
+    </div>
+  );
 }
