8adcc590caefd928e48ed7e55ef425abd414e2c4
Ryan Trimble <mrtrimble@users.noreply.github.com>
2021-12-17 08:50:20 -0500

    added snow to OBS scenes

--- pages/two-cameras.js
+++ pages/two-cameras.js
@@ -3,11 +3,13 @@ import Logo from "../components/Logo";
 import LatestDonations from "../components/LatestDonations";
 import ProgressBar from "../components/ProgressBar";
 import Sponsors from "../components/Sponsors";
+import Snow from '../components/Snow';
 import styles from "../styles/TwoCameras.module.css";
 
 export default function TwoCameras() {
   return (
     <div className="scene">
+      <Snow />
       <div className={styles.host} />
       <div className={styles.guest} />
       <Logo height={154} width={260} className={styles.logo} />
