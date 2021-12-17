d234f68769a17e9f9b750c50ef0f771c59a8b7c0
Ben Myers <ben.myers@okstate.edu>
2021-12-16 23:52:40 -0600

    Get a working queue that leverages fake data

--- components/LatestDonations.js
+++ components/LatestDonations.js
@@ -5,64 +5,101 @@ import styles from "../styles/LatestDonations.module.css";
 import DonationCard from "./DonationCard";
 
 export default function LatestDonations({ className }) {
-  const { data } = useSupabaseDonations();
-  const [fakeData, setFakeData] = useState([]);
-  const [queue, setQueue] = useState([]);
-  const [displayList, setDisplayList] = useState([]);
+	/** @type {{data: TeamSeasDonation[]}} */
+	const { data = [] } = useSupabaseDonations();
 
-  useEffect(() => {
-    if (data) {
-      [...data, ...fakeData].forEach((donation) => {
-        const loggedDonations = [...queue, ...displayList];
-        const donationExists = loggedDonations.find(
-          (loggedDonation) => loggedDonation.id === donation.id
-        );
+	/** @type {[TeamSeasDonation[], function]} */
+	// const [queue, setQueue] = useState([]);
 
-        if (!donationExists) {
-          setQueue([donation, ...queue]);
-          setTimeout(() => {
-            setDisplayList([...displayList, donation]);
-            console.count("Donation added");
-            setQueue(queue.filter((donation) => donation.id !== donation.id));
-          }, 1000);
-        }
-      });
-    }
-  }, [data, displayList, queue, fakeData]);
+	/** @type {Object<number, boolean>} */
+	const [shownDonationIds, setShownDonationIds] = useState({});
 
-  const addDonation = () => {
-    // Add a donation to the list
-    setFakeData([
-      ...fakeData,
-      {
-        display_name: "Leif will find a way",
-        team_name: "Frontend Horse",
-        message_public: "This is a test message!",
-        donation: 5,
-        created_at: Date.now(),
-        id: Math.floor(Math.random() * 10000),
-      },
-    ]);
-  };
+	const [fakeData, setFakeData] = useState([]);
 
-  // let displayList = useMemo(() => {
-  //   return data?.length ? [...data, ...fakeData] : fakeData;
-  // }, [data, fakeData]);
+	let DATA = [...data, ...fakeData]
+	const queue = DATA.filter(donation => !shownDonationIds[donation.id]);
+	const displayList = DATA
+		.slice(-20)
+		.filter(donation => shownDonationIds[donation.id]);
 
-  return (
-    <div className={`${styles.container} ${className}`}>
-      {!!queue.length && <div>{queue[0].display_name}</div>}
-      <button onClick={addDonation}>Add Donation</button>
-      <p className={styles.header}>Latest Donations</p>
-      <div className={styles.listContainer}>
-        {displayList?.sort(compareNumbers).map((item) => {
-          return <DonationCard styles={styles} key={item.id} item={item} />;
-        })}
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		let throttle = setInterval(() => {
+			if (queue.length > 0) {
+				let nextDonation = queue[0];
+				setShownDonationIds(shown => ({...shown, [nextDonation.id]: true}));
+			}
+		}, 1000);
+
+		return () => clearInterval(throttle);
+	}, [queue]);
+	//   const [queue, setQueue] = useState([]);
+	//   const [displayList, setDisplayList] = useState([]);
+
+	//   useEffect(() => {
+	//       [...data, ...fakeData].forEach((donation) => {
+	//         const loggedDonations = [...queue, ...displayList];
+	//         const donationExists = loggedDonations.find(
+	//           (loggedDonation) => loggedDonation.id === donation.id
+	//         );
+
+	//         if (!donationExists) {
+	//           setQueue([donation, ...queue]);
+	//           setTimeout(() => {
+	//             setDisplayList([...displayList, donation]);
+	//             console.count("Donation added");
+	//             setQueue(queue.filter((donation) => donation.id !== donation.id));
+	//           }, 1000);
+	//         }
+	//       });
+	//   }, [data, displayList, queue, fakeData]);
+
+	const addDonation = () => {
+		// Add a donation to the list
+		setFakeData([
+			...fakeData,
+			{
+				display_name: "Leif will find a way",
+				team_name: "Frontend Horse",
+				message_public: "This is a test message!",
+				donation: 5,
+				created_at: Date.now(),
+				id: Math.floor(Math.random() * 10000),
+			},
+		]);
+	};
+
+	// let displayList = useMemo(() => {
+	//   return data?.length ? [...data, ...fakeData] : fakeData;
+	// }, [data, fakeData]);
+
+	return (
+		<div className={`${styles.container} ${className}`}>
+			{!!queue.length && <div>{queue[0].display_name}</div>}
+			<button onClick={addDonation}>Add Donation</button>
+			<p className={styles.header}>Latest Donations</p>
+			<div className={styles.listContainer}>
+				{displayList?.sort(compareNumbers).map((item) => {
+					return <DonationCard styles={styles} key={item.id} item={item} />;
+				})}
+			</div>
+		</div>
+	);
 }
 
 function compareNumbers(a, b) {
-  return b.created_at - a.created_at;
+	return b.created_at - a.created_at;
 }
+
+/**
+ * @typedef {object} TeamSeasDonation
+ * @property {number} id
+ * @property {string} color
+ * @property {number} created_at
+ * @property {string} display_name
+ * @property {number} donation
+ * @property {number} ff
+ * @property {string} flair
+ * @property {string} is_gift
+ * @property {string} message_public
+ * @property {'Frontend Horse'} team_name
+ */
