9052e4372e5a22e6aabb0141a011a0cde2595588
Alex Trost <19617280+a-trost@users.noreply.github.com>
2021-12-17 08:23:18 -0500

    Setup Query params for nametag

--- pages/nametag.js
+++ pages/nametag.js
@@ -1,8 +1,11 @@
-import React from "react";
 import styles from "../styles/Nametag.module.css";
-
+import { useRouter } from "next/router";
 export default function NameTag({ name = "Long Namensteinsmith" }) {
-  // TODO: Wire up to CMS for name
+  const router = useRouter();
+  if (router.query.name) {
+    name = router.query.name;
+  }
+
   return (
     <div className={styles.container}>
       {/* Add tag loop if we have time */}
