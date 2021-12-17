7bb6336464b47d8fd6330f81255f9da87e83bfa9
Leif Niemczik <leif.niem@gmail.com>
2021-12-17 15:19:00 +0100

    intermission

--- pages/intermission-ad.js
+++ pages/intermission-ad.js
@@ -1,6 +1,17 @@
 // Scene for the Intermission ad videos with logo and question
 import React from "react";
+import styles from "../styles/Intermission.module.css"
 
 export default function IntermissionAd() {
-  return <div>Intermission Ad</div>;
+  return(<div className={styles.container}>
+	  <video src="/sleigh-loop.webm" className={styles.video} autoPlay loop muted></video>
+	  <div className={styles.overlay}>
+		  <div className="title">
+			  <h1>Intermission!</h1>
+			  <h4>
+							We will be back in a second.
+			  </h4>
+		  </div>
+	  </div>
+  </div>);
 }
--- pages/nametag.js
+++ pages/nametag.js
@@ -5,7 +5,7 @@ export default function NameTag({ name = "Long Namensteinsmith" }) {
   // TODO: Wire up to CMS for name
   return (
     <div className={styles.container}>
-      {/* Add tag loop if we have time */}
+      <svg className={styles.loop} width="50" height="100" viewBox="0 0 50 100" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M50.666.774c-27.614 0-50 22.386-50 50 0 27.615 22.386 50 50 50v-100ZM28.252 58.246a9.195 9.195 0 1 0 0-18.391 9.195 9.195 0 0 0 0 18.39Z"/></svg>
       <div className={styles.inner}>{name}</div>
     </div>
   );
